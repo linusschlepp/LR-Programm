eac1a35cac788392f17ee600aaf93a6aa3dcb64a
linus <linusmo.94@googlemail.com>
2022-02-14 20:07:35 +0100

    Add listeners

--- src/main/java/StartBox.java
+++ src/main/java/StartBox.java
@@ -1,17 +1,18 @@
-import javafx.beans.binding.Bindings;
-import javafx.beans.binding.StringBinding;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
-import javafx.scene.text.Font;
-
 import static javafx.scene.text.Font.*;
-
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
+import java.util.concurrent.atomic.AtomicInteger;
 import static javafx.scene.text.FontPosture.*;
 import static javafx.scene.text.FontWeight.*;
 import static java.lang.Integer.*;
@@ -20,8 +21,9 @@ import static java.lang.Integer.*;
 public class StartBox {
 
     static Stage primaryStage;
-
-
+    static AtomicInteger xCord = new AtomicInteger(1);
+    static AtomicInteger yCord = new AtomicInteger(5);
+    static GridPane gridPane = new GridPane();
 
 
     public static void display(Stage primaryStage) {
@@ -29,26 +31,51 @@ public class StartBox {
 
 
         //Layout
-        GridPane gridPane = new GridPane();
-        gridPane.setVgap(8);
-        gridPane.setHgap(10);
+        gridPane.getColumnConstraints().add(new ColumnConstraints(50));
+        gridPane.getRowConstraints().add(new RowConstraints(25));
+        gridPane.setVgap(25);
+        gridPane.setHgap(75);
+        //gridPane.setGridLinesVisible(true);
 
         //Labels
-        Label label = new Label("How many periods do you want to enter?");
-        GridPane.setConstraints(label, 0, 1);
+        Label label = new Label("Enter your needs: ");
+        GridPane.setConstraints(label, 0, 1, 1,1);
+
+        //ComboBox
+        ComboBox comboBox = new ComboBox(FXCollections.observableArrayList(1,2,3,4,5));
+        GridPane.setConstraints(comboBox, 0,4);
+        gridPane.getChildren().add(comboBox);
+//        comboBox.valueProperty().addListener(e -> {
+//            int temp = yCord.get();
+//            temp -= 3;
+//            TextField t2 = new TextField();
+//            t2.setPrefWidth(60);
+//            t2.setMaxWidth(60);
+//            Label label1 = new Label("n"+temp);
+//            yCord.addAndGet(1);
+//            GridPane.setConstraints(t2, 1, yCord.get());
+//            GridPane.setConstraints(label1, 0, yCord.get());
+//            gridPane.getChildren().addAll(t2, label1);
+//        });
+        comboBox.valueProperty().addListener();
+
+
 
 
         //TextFields
         TextField textField = new TextField();
-        textField.setPrefWidth(80);
-        textField.setMaxWidth(80);
-        GridPane.setConstraints(textField, 0, 2);
+        textField.setPrefWidth(60);
+        textField.setMaxWidth(60);
+        GridPane.setConstraints(textField, 0, 3);
 
         //text
         Text mainText = new Text(20, 50, "Welcome to the linear regression calculator!");
-        Font font = font("Calibri", BOLD, ITALIC, 18);
-        mainText.setFont(font);
+        mainText.setFont(font("Calibri", BOLD, ITALIC, 18));
         GridPane.setConstraints(mainText, 0, 0);
+
+
+
+        specialListener(textField);
 
 
         //Button
@@ -57,17 +84,34 @@ public class StartBox {
             PeriodBox.display(parseInt(textField.getText()));
             primaryStage.close();
         });
-        GridPane.setConstraints(button, 0, 3);
+        GridPane.setConstraints(button, 0, 15);
 
 
         //final setup of scene and stage
         gridPane.getChildren().addAll(label, textField, mainText, button);
-        Scene scene = new Scene(gridPane, 500, 300);
+        Scene scene = new Scene(gridPane, 1000, 700);
         primaryStage.setTitle("LR-Program");
         primaryStage.setScene(scene);
         primaryStage.show();
 
 
+    }
 
+    static void specialListener(TextField t1) {
+
+        ChangeListener<String> changeListener = new ChangeListener<>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                TextField t2 = new TextField();
+                t2.setPrefWidth(60);
+                t2.setMaxWidth(60);
+                GridPane.setConstraints(t2, xCord.get(), 3);
+                xCord.getAndIncrement();
+                gridPane.getChildren().add(t2);
+                t1.textProperty().removeListener(this);
+                specialListener(t2);
+            }
+        };
+        t1.textProperty().addListener(changeListener);
     }
 }
