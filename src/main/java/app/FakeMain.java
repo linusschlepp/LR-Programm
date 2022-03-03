eb53ea488a97efaf4031a888d05fd2fcc128aead
linus <linusmo.94@googlemail.com>
2022-03-03 21:05:49 +0100

    Added FakeMain.java to create a jar

---
+++ src/main/java/app/FakeMain.java
@@ -0,0 +1,13 @@
+package app;
+
+public class FakeMain {
+
+    /**
+     * This class only exists for the purpose of creating a .jar file. It starts the actual Main
+     *
+     * @param args from cmd
+     */
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
